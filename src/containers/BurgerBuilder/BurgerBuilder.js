a1e3c488a24b802d70c3fb8a5a6dc96e7da3c8bf
reiahx <ahnreiwilliams@gmail.com>
2018-09-28 09:01:38 -0400

    UI components coming in

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,6 +71,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
 
+    purchaseContinueHandler = () => {
+        alert('You continue');
+    }
+
     render() {
         const disabledInfo = { ...this.state.ingredients };
         for(let key in disabledInfo) {
@@ -79,7 +83,11 @@ class BurgerBuilder extends Component {
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        price={this.state.totalPrice} 
+                        ingredients={this.state.ingredients}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
